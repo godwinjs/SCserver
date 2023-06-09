6dca172968a57434c179cf585dd592db51dacb58
devtrills <godwinikechukwu.dev@gmail.com>
2023-06-09 19:23:32 +0100

    upd

--- duplicates/dp_index.js
+++ duplicates/dp_index.js
@@ -21,7 +21,7 @@ app.get('/url?*', function (req, res) {
     let sheetArr = [];
     let newSheet = null;
     //
-    const sheets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const sheets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
     const objects = []
     const schema = [
     {
