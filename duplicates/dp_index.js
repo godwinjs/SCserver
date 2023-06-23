2cb351e2d2c3b0bd584aba48055bb1cc7899bea6
devtrills <godwinikechukwu.dev@gmail.com>
2023-06-23 10:49:25 +0100

    Update dp_index.js

--- duplicates/dp_index.js
+++ duplicates/dp_index.js
@@ -21,11 +21,7 @@ app.get('/url?*', function (req, res) {
     let sheetArr = [];
     let newSheet = null;
     //
-<<<<<<< Updated upstream
     const sheets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,23,24, 25, 26, 27];
-=======
-    const sheets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,23,24, 25];
->>>>>>> Stashed changes
     const objects = []
     const schema = [
     {
