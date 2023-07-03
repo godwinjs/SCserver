5a9b969b5eda8c4d89f7d0755cc6000975eb42fe
devtrills <godwinikechukwu.dev@gmail.com>
2023-07-03 22:25:15 +0100

    upd

---
+++ casinoVerify/ai_index.js
@@ -0,0 +1,35 @@
+const apiKey = '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';
+const endpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+async function getChatResponse(prompt) {
+  const requestBody = {
+    prompt: prompt,
+    max_tokens: 50, // Adjust as needed to control the response length
+    temperature: 0.7, // Adjust as needed to control the response randomness
+    n: 1, // Number of responses to generate
+    stop: '\n', // Stop generating tokens at a newline character
+  };
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+  const data = await response.json();
+  const chatResponse = data.choices[0].text.trim();
+  return chatResponse;
+}
+
+//
+const prompt = 'respond with true or false if this site elroyalecasino.com is a casino site?';
+getChatResponse(prompt)
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
