ab234ab53934b504a0f5c14973a59187f53133a2
devtrills <godwinikechukwu.dev@gmail.com>
2023-07-27 15:57:23 +0100

    Update notes.js

--- notes.js
+++ notes.js
@@ -120,3 +120,257 @@ const sendCrawlRequest = (url) => {
 Promise.all(links).then((link) => {
   // link[0].property, link[1].property
 })
+
+
+var data = [
+  `
+The Columbus Dispatch - 
+Cleveland.com - 
+The Cincinnati Enquirer - 
+Dayton Daily News - 
+Akron Beacon Journal - 
+The Blade - 
+The Plain Dealer - 
+WDTN-TV - 
+The Columbus Free Press - 
+WKSU - 
+The Cincinnati Business Courier - 
+The Toledo Business Journal - 
+The Cincinnati CityBeat - 
+Columbus CEO - 
+The Dayton Business Journal - 
+Dayton.com - 
+The Cincinnati City Council - 
+The Cleveland Scene - 
+The Toledo City Paper - 
+The Cleveland Jewish News - 
+The Cincinnati City Council - 
+The Akron City Council - 
+The Toledo City Council - 
+The Cleveland Business News - 
+The Cincinnati Herald - 
+The Dayton City Paper - 
+The Canton Repository - 
+The Youngstown Vindicator - 
+The Springfield News-Sun - 
+The Lima News - 
+The Marietta Times - 
+The Mansfield News Journal - 
+The News-Herald - 
+The Journal-News - 
+The Warren Tribune Chronicle - 
+The Findlay Courier - 
+The Morning Journal - 
+The Zanesville Times Recorder - 
+The Athens Messenger - 
+The Delaware Gazette - 
+The Chillicothe Gazette - 
+The Alliance Review - 
+The Sandusky Register - 
+The Lancaster Eagle-Gazette - 
+The Wooster Daily Record - 
+The Defiance Crescent-News - 
+The Ashland Times-Gazette - 
+The Norwalk Reflector - 
+The Galion Inquirer - 
+The Salem News - 
+The Steubenville Herald-Star - 
+The Ironton Tribune - 
+The Portsmouth Daily Times - 
+The Tiffin Advertiser-Tribune - 
+The Van Wert Times Bulletin - 
+The Circleville Herald - 
+The Piqua Daily Call - 
+The Sidney Daily News - 
+The Xenia Daily Gazette - 
+The Bellefontaine Examiner - 
+The Willard Times-Junction - 
+The Bryan Times - 
+The Mount Vernon News - 
+The Newark Advocate - 
+The Fostoria Review Times - 
+The Kent Record-Courier - 
+The Marysville Journal-Tribune - 
+The Wilmington News Journal - 
+The Wapakoneta Daily News - 
+The Paulding Progress - 
+The Willoughby News-Herald - 
+The Norwalk Reflector - 
+The Lima News - 
+The Marion Star - 
+The News-Messenger - 
+The Xenia Gazette - 
+The Cuyahoga Falls News-Press - 
+The Greenfield Times-Gazette - 
+The West Side Leader - 
+The Bellefontaine Examiner - 
+The Willard Times-Junction - 
+The Van Wert Times Bulletin - 
+The Circleville Herald - 
+The Piqua Daily Call - 
+The Sidney Daily News - 
+The Xenia Daily Gazette - 
+The Bryan Times - 
+The Mount Vernon News - 
+The Newark Advocate - 
+The Fostoria Review Times - 
+The Kent Record-Courier - 
+The Marysville Journal-Tribune - 
+The Wilmington News Journal - 
+The Wapakoneta Daily News - 
+The Paulding Progress - 
+The Willoughby News-Herald - 
+The Jackson County Times-Journal - 
+The New Philadelphia Times-Reporter - 
+The Columbus Dispatch - 
+Cleveland.com - 
+The Cincinnati Enquirer - 
+Dayton Daily News - 
+Akron Beacon Journal - 
+The Blade - 
+The Plain Dealer - 
+WDTN-TV - 
+The Columbus Free Press - 
+WKSU - 
+The Cincinnati Business Courier - 
+The Toledo Business Journal - 
+The Cincinnati CityBeat - 
+Columbus CEO - 
+The Dayton Business Journal - 
+Dayton.com - 
+The Cincinnati City Council - 
+The Cleveland Scene - 
+The Toledo City Paper - 
+The Cleveland Jewish News - 
+The Cincinnati City Council - 
+The Akron City Council - 
+The Toledo City Council - 
+The Cleveland Business News - 
+The Cincinnati Herald - 
+  `,
+  `
+  https://www.dispatch.com/
+https://www.cleveland.com/
+https://www.cincinnati.com/
+https://www.daytondailynews.com/
+https://www.beaconjournal.com/
+https://www.toledoblade.com/
+https://www.cleveland.com/plaindealer/
+https://www.wdtn.com/
+https://columbusfreepress.com/
+https://www.wksu.org/
+https://www.bizjournals.com/cincinnati/
+https://www.toledobiz.com/
+https://www.citybeat.com/
+https://www.columbusceo.com/
+https://www.bizjournals.com/dayton/
+https://www.dayton.com/
+https://www.cincinnati-oh.gov/council/
+https://www.clevescene.com/
+https://toledocitypaper.com/
+https://www.clevelandjewishnews.com/
+https://www.cincinnati-oh.gov/council/
+https://www.akroncitycouncil.org/
+https://toledo.legistar.com/
+https://www.crainscleveland.com/
+https://thecincinnatiherald.com/
+https://www.daytoncitypaper.com/
+https://www.cantonrep.com/
+https://www.vindy.com/
+https://www.springfieldnewssun.com/
+https://www.limaohio.com/
+https://www.mariettatimes.com/
+https://www.mansfieldnewsjournal.com/
+https://www.news-herald.com/
+https://www.journal-news.com/
+https://www.tribtoday.com/
+https://thecourier.com/
+https://www.morningjournal.com/
+https://www.zanesvilletimesrecorder.com/
+https://www.athensmessenger.com/
+https://www.delgazette.com/
+https://www.chillicothegazette.com/
+https://www.the-review.com/
+https://www.sanduskyregister.com/
+https://www.lancastereaglegazette.com/
+https://www.the-daily-record.com/
+https://www.crescent-news.com/
+https://www.times-gazette.com/
+https://www.norwalkreflector.com/
+https://www.galioninquirer.com/
+https://www.salemnews.net/
+https://www.heraldstaronline.com/
+https://www.irontontribune.com/
+https://www.portsmouth-dailytimes.com/
+https://www.advertiser-tribune.com/
+https://timesbulletin.com/
+https://www.circlevilleherald.com/
+https://www.dailycall.com/
+https://www.sidneydailynews.com/
+https://www.xeniagazette.com/
+https://www.examiner.org/
+https://www.timesjunction.com/
+https://www.bryantimes.com/
+https://mountvernonnews.com/
+https://www.newarkadvocate.com/
+https://reviewtimes.com/
+https://www.record-courier.com/
+https://www.marysvillejt.com/
+https://www.wnewsj.com/
+https://www.wapakdailynews.com/
+https://progressnewspaper.org/
+https://www.news-herald.com/
+https://www.norwalkreflector.com/
+https://www.limaohio.com/
+https://www.marionstar.com/
+https://www.thenews-messenger.com/
+https://www.xeniagazette.com/
+https://www.fallsnewspress.com/
+https://www.timesgazette.com/
+https://www.akron.com/
+https://www.examiner.org/
+https://www.timesjunction.com/
+https://timesbulletin.com/
+https://www.circlevilleherald.com/
+https://www.dailycall.com/
+https://www.sidneydailynews.com/
+https://www.xeniagazette.com/
+https://www.bryantimes.com/
+https://mountvernonnews.com/
+https://www.newarkadvocate.com/
+https://reviewtimes.com/
+https://www.record-courier.com/
+https://www.marysvillejt.com/
+https://www.wnewsj.com/
+https://www.wapakdailynews.com/
+https://progressnewspaper.org/
+https://www.news-herald.com/
+https://thetelegramnews.com/
+https://www.timesreporter.com/
+https://www.dispatch.com/
+https://www.cleveland.com/
+https://www.cincinnati.com/
+https://www.daytondailynews.com/
+https://www.beaconjournal.com/
+https://www.toledoblade.com/
+https://www.cleveland.com/plaindealer/
+https://www.wdtn.com/
+https://columbusfreepress.com/
+https://www.wksu.org/
+https://www.bizjournals.com/cincinnati/
+https://www.toledobiz.com/
+https://www.citybeat.com/
+https://www.columbusceo.com/
+https://www.bizjournals.com/dayton/
+https://www.dayton.com/
+https://www.cincinnati-oh.gov/council/
+https://www.clevescene.com/
+https://toledocitypaper.com/
+https://www.clevelandjewishnews.com/
+https://www.cincinnati-oh.gov/council/
+https://www.akroncitycouncil.org/
+https://toledo.legistar.com/
+https://www.crainscleveland.com/
+https://thecincinnatiherald.com/
+  `
+]
