7b901dd93145a895db71acd0ba7d4e20a39723cb
devtrills <godwinikechukwu.dev@gmail.com>
2023-08-08 09:17:52 +0100

    ordinary member
    All problems with ordinary members solved.

--- brunoJob/bn_index.js
+++ brunoJob/bn_index.js
@@ -1,27 +1,9 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
-// const crawler = require("cheerio-crawler")
 const express = require('express')
 const readXlsxFile = require('read-excel-file/node')
 const writeXlsxFile = require('write-excel-file/node')
 
-// const http = require('https'); // or 'https' for https:// URLs
-// const fs = require('fs');
-
-// var download = function(url, dest, cb) {
-//     var file = fs.createWriteStream(dest);
-//     var request = http.get(url, function(response) {
-//       response.pipe(file);
-//       file.on('finish', function() {
-//         file.close(cb);  // close() is async, call cb after close completes.
-//       });
-//     }).on('error', function(err) { // Handle errors
-//       fs.unlink(dest); // Delete the file async. (But we don't check the result)
-//       if (cb) cb(err.message);
-//     });
-//   };
-
-// 
 const app = express()
 
 // Cors error bypass
@@ -31,52 +13,40 @@ app.use((req, res, next) => {
   })
 
 app.get('/url', function (req, res) {
-    // download("https://docs.google.com/spreadsheets/d/1nUPJ42G2lEWl4akz1LC_aNe9oC67fR7qnDM3qEy9jhQ/edit#gid=2004427024", "BrunoExcel.xlsx", callback)
 
         let sheetArr = [];
-        let newSheet = null;
-        let editedObj = [];
         //
-        const sheets = [1, 2];
+        const sheets = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         const spouseObjects = []
         const ordinaryObjects = []
-
-      
-        // console.log(queryObject.sport);
 
     
         // "C:\Users\godwi\Downloads\LinkSwapsSimilarSites.xlsx"
         async function writeToExcelFile(arr) {
-            // arr.splice(0, 1)
             arr.map((row) => {
-                // console.log(row[7])
     
-            spouseObjects.push({
-                category: row[0] == null ? '' : row[0]+'',
-                id: row[1] == null ? '' : row[1]+'',
-                name: row[2] == null ? '' : row[2],
-                section: row.section == null ? '' : row.section,
-                gender:  row.gender == null ? '' : row.gender,
-                country:   row.country == null ? '' : row.country,
-                phone: row.phone === null ? 0 : row.phone,
-                email:  row.email === null ? '' : row.email,
-                status: row.status == null ? '' : row.status
-            })
-            // console.log(row[6].toString())
-            // if(row.phone == '8022220222'){
-            //     console.log(typeof(row.phone))
-            // }
-            ordinaryObjects.push({
-                memberId:  row.memberId == null ? '' : row.memberId+'',
-                surname:  row.surname == null ? '' : row.surname,
-                firstname:  row.firstname == null ? '' : row.firstname,
-                section: row.section == null ? '' : row.section,
-                gender:  row.gender == null ? '' : row.gender,
-                country:   row.country == null ? '' : row.country,
-                phone: row.phone === null ? '0' : row.phone,
-                email:  row.email === null ? '' : row.email,
-                status: row.status == null ? '' : row.status
-            })
+                spouseObjects.push({
+                    category: row[0] == null ? '' : row[0]+'',
+                    id: row[1] == null ? '' : row[1]+'',
+                    name: row[2] == null ? '' : row[2],
+                    section: row.section == null ? '' : row.section,
+                    gender:  row.gender == null ? '' : row.gender,
+                    country:   row.country == null ? '' : row.country,
+                    phone: row.phone === null ? 0 : row.phone,
+                    email:  row.email === null ? '' : row.email,
+                    status: row.status == null ? '' : row.status
+                })
+                ordinaryObjects.push({
+                    memberId:  row[0] == null ? '' : row[0]+'',
+                    surname:  row[1] == null ? '' : row[1],
+                    firstname:  row[2] == null ? '' : row[2],
+                    gender:  row[3] == null ? '' : row[3],
+                    country:   row[4] == null ? '' : row[4],
+                    phone: row[5] === null ? '0' : `${row[5]}`,
+                    email:  row[6] === null ? '' : row[6],
+                    status: row[7] == null ? '' : row[7],
+                    section: row[8] == null ? '' : row[8]
+                })
     
             })
 
@@ -152,11 +122,6 @@ app.get('/url', function (req, res) {
                         value: row => row.firstname
                     },
                     {
-                        column: 'SECTION',
-                        type: String,
-                        value: row => row.section
-                    },
-                    {
                         column: 'GENDER',
                         type: String,
                         value: row => row.gender
@@ -180,6 +145,11 @@ app.get('/url', function (req, res) {
                         column: 'STATUS',
                         type: String,
                         value: row => row.status
+                    },
+                    {
+                        column: 'SECTION',
+                        type: String,
+                        value: row => row.section
                     }
                 ]
                 await writeXlsxFile(ordinaryObjects, {
@@ -215,54 +185,48 @@ app.get('/url', function (req, res) {
                         status: b_row[7],
                     }
                     if(stringsMatch(a_row[1], obj.memberId[1])){
-                        if(b_row[1] == null){
-                            return;
-                        }
                         if(obj.section === null){
                             obj.section = `(${req.query.sport.toUpperCase()})`
                         }else{
                             obj.section = obj.section.toString() + `(${req.query.sport.toUpperCase()})`
                         }
-                        // b_row.push(`(${req.query.sport.toUpperCase()})`)
-                        editedObj.push(obj);
+                        editedObj.push(arr);
                     }
                 })
             })
         }
         function ordinary(a, b){
-            // console.log([b[0], b[1], b[2]])
             a.map((a_row) => {
-    
-                b.map((b_row) => {
-                    let obj = {
-                        memberId: b_row[0],
-                        surname: b_row[1],
-                        firstname: b_row[2],
-                        section: null,
-                        gender: b_row[3],
-                        country: b_row[4],
-                        phone: `${b_row[5]}`,
-                        email: b_row[6],
-                        status: b_row[7],
+                
+                b.map((b_row, idx) => {
+                    // 
+                    if(stringsMatch(a_row[1], b_row[0])){      
+                        if(b_row[8] === undefined){
+                            b_row.push(`(${req.query.sport.toUpperCase()})`)
+                            return;
+                        }
+                        if(b_row[8] === null){
+                            b_row[8] = `(${req.query.sport.toUpperCase()})`
+                            return;
+                        }
+                            b_row[8] = `${b_row[8]}(${req.query.sport.toUpperCase()})`;
+                            // b[idx] = [b_row[0], b_row[1], b_row[2], b_row[3], b_row[4], b_row[5], b_row[6], b_row[7], b[idx][8] + req.query.sport.toUpperCase() ]
+                            // obj.section = obj.section.toString() + `(${req.query.sport.toUpperCase()})`
+                            // b.splice(idx, 1, [
+                            //     ...b_row.slice(0, 7),
+                            //     `${b[idx][8] + req.query.sport.toUpperCase()}`
+                            // ])
+
                     }
-                    if(stringsMatch(a_row[1], obj.memberId)){                        
-                    if(obj.section === null){
-                        obj.section = `(${req.query.sport.toUpperCase()})`
-                    }else{
-                        obj.section = obj.section.toString() + `(${req.query.sport.toUpperCase()})`
-                    }
-                    }
-                    editedObj.push(obj);
                 })
             })
         }
-        
+
         const checkPromise = sheets.map((sheet) => {
             return readXlsxFile("./brunoJob/BrunoExcel.xlsx", {sheet: sheet});
         })
     
         Promise.all(checkPromise).then((rows) => {
-            // console.log(rows)
             rows.map(row => {
                 sheetArr.push(row)
             })
@@ -273,18 +237,17 @@ app.get('/url', function (req, res) {
                 console.log('s')
                 spouse(sheetArr[0], sheetArr[1])
             }else{
-                console.log('o')
-                ordinary(sheetArr[0], sheetArr[1])
+                ordinary(sheetArr[parseInt(req.query.sheet)], sheetArr[0])
             }
             // 
-            console.log('Writing unique entries to excel file...')
-            writeToExcelFile(editedObj);
-        // console.log(editedObj[editedObj.length - 3], editedObj[editedObj.length - 2], editedObj[editedObj.length - 1])
+            console.log(`Writing ${sheetArr[0].length} entries to excel file...`)
+            console.log([sheetArr[0][0], sheetArr[0][1], sheetArr[0][2], sheetArr[0][18], sheetArr[0][19] ])
+            writeToExcelFile(sheetArr[0]);
             console.log('All done file ready.')
         });
         
     res.send({
-        "Success": "Operation Completed and duplicate entries have been removed! \n Enjoy your unique excel file.",
+        "Success": "Operation Completed! \n Enjoy your unique excel file.",
         "arguments": req.query
     })
 })
