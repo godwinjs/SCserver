15a6fb14594d37fe52a4ba0985a96fc0b49a3f1e
Honeypot <godwinikechukwu.dev@gmail.com>
2023-09-18 11:00:38 -0700

    upd

--- server.js
+++ server.js
@@ -5,12 +5,9 @@ const express = require('express')
 const readXlsxFile = require('read-excel-file/node')
 const writeXlsxFile = require('write-excel-file/node')
 
-
+// Table Tennis
 // 
 const app = express()
-
-// keywords for writer/author/sports editor/ editorial/ 
-// package that uses email addresses to get the email owner's name. 
 
 // Cors error bypass
 app.use((req, res, next) => {
@@ -18,246 +15,148 @@ app.use((req, res, next) => {
     next();
   })
 
-
-
 app.get('/url?*', function (req, res) {
+  
+    let allRows = []; 
+    let sheetArr = [];
+    let newSheet = null;
+    //
+    const sheets = [1, 2];
+    const objects = []
+    const schema = [
+    {
+        column: 'Domain',
+        type: String,
+        value: row => row.domain
+    },
+    {
+        column: 'URL',
+        type: String,
+        value: row => row.url
+    },
+    {
+        column: 'Page As',
+        type: Number,
+        value: row => row.pageAs
+    },
+    {
+        column: 'Ref.Domain',
+        type: Number,
+        value: row => row.refDomain
+    },
+    {
+        column: 'Backlinks',
+        type: Number,
+        value: row => row.backlinks
+    },
+    {
+        column: 'Search Traffic',
+        type: Number,
+        value: row => row.searchTraffic
+    },
+    {
+        column: 'URL Keywords',
+        type: Number,
+        value: row => row.urlKeywords
+    },
+    {
+        column: 'Name',
+        type: String,
+        value: row => row.name
+    },
+    {
+        column: 'Email',
+        type: String,
+        value: row => row.email
+    },
+    {
+        column: 'Email Verified',
+        type: Boolean,
+        value: row => row.emailVerified 
+    } 
+    ]
 
-  // console.log(req.query)
-  const { url, fileUrl } = req.query;
-
-  // let allUrls = [];
-  let allRows = [];
-
-  // File path.
-  // .xlsx
-const objects = []
-const schema = [
-  {
-    column: 'Domain',
-    type: String,
-    value: row => row.domain
-  },
-  {
-    column: 'URL',
-    type: String,
-    value: row => row.url
-  },
-  {
-    column: 'Page As',
-    type: Number,
-    value: row => row.pageAs
-  },
-  {
-    column: 'Ref.Domain',
-    type: Number,
-    value: row => row.refDomain
-  },
-  {
-    column: 'Backlinks',
-    type: Number,
-    value: row => row.backlinks
-  },
-  {
-    column: 'Search Traffic',
-    type: Number,
-    value: row => row.searchTraffic
-  },
-  {
-    column: 'URL Keywords',
-    type: Number,
-    value: row => row.urlKeywords
-  },
-  {
-    column: 'Name',
-    type: String,
-    value: row => row.name
-  },
-  {
-    column: 'Email',
-    type: String,
-    value: row => row.email
-  },
-  {
-    column: 'Email Verified',
-    type: Boolean,
-    value: row => row.emailVerified 
-  } 
-]
-  // "C:\Users\godwi\Downloads\LinkSwapsSimilarSites.xlsx"
-  async function writeToExcelFile(arr) {
-    arr.splice(0, 1)
-    arr.map((row) => {
+    // "C:\Users\godwi\Downloads\LinkSwapsSimilarSites.xlsx"
+    async function writeToExcelFile(arr) {
+        arr.splice(0, 1)
+        arr.map((row) => {
 
 
-      objects.push({
-        domain: row[0],
-        url: row[1] + "",
-        pageAs: row[2] === null ? 0 : row[2],
-        refDomain: row[3] === null ? 0 : row[3],
-        backlinks:  row[4] === null ? 0 : row[4],
-        searchTraffic:  row[5] === null ? 0 : row[5],
-        urlKeywords:  row[6] === null ? 0 : row[6],
-        name: row[7],
-        email: row[8],
-        emailVerified: row[9]
-      })
+        objects.push({
+            domain: row[0],
+            url: row[1] + "",
+            pageAs: row[2] === null ? 0 : row[2],
+            refDomain: row[3] === null ? 0 : row[3],
+            backlinks:  row[4] === null ? 0 : row[4],
+            searchTraffic:  row[5] === null ? 0 : row[5],
+            urlKeywords:  row[6] === null ? 0 : row[6],
+            name: row[7],
+            email: row[8],
+            emailVerified: row[9]
+        })
 
-    })
-    // console.log(objects[0])
-    // console.log(allRows[0])
-
-      await writeXlsxFile(objects, {
-        schema,
-        filePath: './link.xlsx'
-      })
-  }
-
-  readXlsxFile("./AllKeyword.xlsx", {sheet: 1}).then((rows) => {
-    // `rows` is an array of rows
-    // each row being an array of cells.
-    // rows.map((row) => {
-    //   allUrls.push(row[0])
-    //   // allRows.push(row)
-    //   // console.log(row)
-    // })
-    allRows = rows;
-    let count = 0;
-    console.log(allRows.length)
-    allRows.map((rowCheck, idxCheck) => {   
-       
-      let rep = 0;
-
-      allRows.map((rowAgainst, idxAgainst) => {
-        count = count + 1;   
-        if(rowCheck[0] === rowAgainst[0]){
-          if(rep >= 1){
-            allRows.splice(idxAgainst, 1)
-          }
-          rep = rep + 1;
-        }
-        if(allRows.length - 1 == idxCheck){
-          console.log(count)
-        }
-      })
-
-    })
-    console.log(allRows.length)
-    console.log(allRows[allRows.length - 1])
-    writeToExcelFile(allRows)
-  })
-
-
-  // readXlsxFile("./LinkSwapsSimilarSites.xlsx", {sheet: 3}).then((rows) => {
-  //   // `rows` is an array of rows
-  //   // each row being an array of cells.
-  //   rows.map((row) => {
-  //     // console.log(row[1])
-  //     allUrls.push(row[1])
-  //   })
-  //   console.log(allUrls.length)
-  //   console.log(allUrls[allUrls.length - 1])
-  //   allUrls.shift()
-  //   // getDom(allUrls)
-
-  // })
-  const crawledData = [];
-
-  const crawlDom = (response) => {
-    // link[0].property, link[1].property
-    let url;
-    if(response.value.config.url === undefined){
-      url = 'bad url'
-    }else{
-      url = response.value.config.url;
-    }
-
-      let dom = response.value.data;
-        const $ = cheerio.load(dom)
-
-        // plugins
-        $.prototype.logHtml = function () {
-          console.log(this.html());
-        };
-        $.prototype.logText = function () {
-          console.log(this.text());
-        };
-
+        })
         // 
-        $('body').find('a').map((a) => {
-          let href = $('body').find('a')[a].attribs.href;
-          // console.log(href.search(/contact/i))
-          if(!(href === undefined)){
-            if(href.search(/contact/i) > -1) {
-              crawledData.push({[url]: href})
-            }
-            if(href.search(/mailto/i) > -1){
-              crawledData.push({[url]: href})
-            }
-            if(href.search(/author/i) > -1){
-              crawledData.push({[url]: href})
-            }
-            if(href.search(/writer/i) > -1){
-              crawledData.push({[url]: href})
-            }
-            if(href.search(/editor/i) > -1){
-              crawledData.push({[url]: href})
-            }
-            if(href.search(/writing/i) > -1){
-              crawledData.push({[url]: href})
-            }
-            if(href.search(/about/i) > -1){
-              // crawledData.push({[url]: href})
-            }
-            if(href.search(/about-us/i) > -1){
-              // crawledData.push({[url]: href})
-            }
-            if(href.search(/contact-us/i) > -1){
-              crawledData.push({[url]: href})
-            }
-      }
-    })
-    // promise.value.config.url
-    console.log(crawledData)
-    
-  }
 
-  const getDom = (urls) => {
-    // for url array batch processing
-    const sendCrawlRequest = (url) => {
-      return axios.get(url).catch(error => {
-        console.error('Error:', error.message)
-        console.error('Stack trace:', error.stack)
-  
-        // Retry the request
-        console.log(`Retrying request to ${url}...`)
-        return axios.get(url)
-
-      }).finally(() => {
-        // console.log(url)
-      })
+        await writeXlsxFile(objects, {
+            schema,
+            filePath: './sheet.xlsx'
+        })
     }
-    const links = urls.map((url) => {
-      return sendCrawlRequest(url)
+    function removeDuplicates(existingSheet, newSheet) {
+        // existingSheet.shift();
+
+        existingSheet.map((existingRow, idxExist) => {   
+      
+            newSheet.map((newRow, idxNew) => {
+                let rep = 0;
+
+                if(existingRow[0] === newRow[0]){
+                    // console.log(newRow[0])
+                    newSheet.splice(idxNew, 1)
+                    // console.log(newSheet[idxNew])
+                }
+                newSheet.map((rowAgainst, idxAgainst) =>{
+                    if(newRow[0] === rowAgainst[0]){
+                        if(rep >= 1){
+                            console.log(newRow[0])
+                          newSheet.splice(idxAgainst, 1)
+                        }
+                        rep = rep + 1;
+                      }
+                })
+            })
+      
+          })
+    }
+    
+    const checkPromise = sheets.map((sheet) => {
+        return readXlsxFile("./SingleSheet.xlsx", {sheet: sheet});
     })
 
-    Promise.allSettled(links).then((results) => {
-      const fulfilledResponses = results.filter(result => result.status === 'fulfilled');
-      const rejectedResponses = results.filter(result => result.status === 'rejected');
+    Promise.all(checkPromise).then((rows) => {
+        rows.map((row, i) => {
+            sheetArr.push(row)
+        })
+    }).finally(() => {
+        if(newSheet === null){
+            newSheet = sheetArr[sheetArr.length - 1]
+            console.log(newSheet.length)
+            // console.log(sheetArr[sheetArr.length - 1][0])
+            sheetArr.pop();
+        }
 
-      fulfilledResponses.map((promise, index) => {
-        crawlDom(promise, index)
-      })
-    })
-
-  }
-  
-  res.send({"Success": "Operation Completed and duplicate entries have been removed! \n Enjoy your unique excel file."})
-
-})
-
-app.get('/urls?*', function (req, res) {
-  console.log(req.query)
+        sheetArr.map((existingSheet) => {
+            removeDuplicates(existingSheet, newSheet);
+        })
+        console.log('Writing unique entries to excel file...')
+        writeToExcelFile(newSheet);
+        console.log(newSheet.length)
+        console.log('All done file ready.')
+    });
+        
+    res.send({"Success": "Operation Completed and duplicate entries have been removed! \n Enjoy your unique excel file."})
 })
 
     
-app.listen(5001)
+app.listen(5000)
